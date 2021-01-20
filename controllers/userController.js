2750a541373c139fe8a91533eeca8832dbac4a64
AndreaBLS <frostbittenblash@gmail.com>
2021-01-20 11:35:50 +0100

    building aws upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,24 +2,23 @@ const User = require("../models/userModel")
 const { registerValidation, loginValidation } = require("../middlewares/validator")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
+const createError = require('http-errors')
 
 exports.userRegister = async (req, res) => {
-    // validating data before creating user
+
     const { error } = registerValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    //Checking if the email exists in our DB
     const emailExist = await User.findOne({ email: req.body.email })
     if (emailExist) return res.status(400).send("Email already exists in our database")
 
-    //Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-    //Create a new user
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
+        userName: req.body.userName,
         email: req.body.email,
         password: hashedPassword
     })
@@ -34,28 +33,28 @@ exports.userRegister = async (req, res) => {
 exports.userLogin = async (req, res) => {
     try {
         const { error } = loginValidation(req.body)
-        console.log(req.body)
         if (error) return res.status(400).send(error.details[0].message)
 
-        //Checking if the email exists in our DB
         const user = await User.findOne({ email: req.body.email })
-        console.log(user)
+
         if (!user) return res.status(400).send("Email not found,disclosing that information just for testing purposes")
 
-        //Is password correct?
         const validPassword = await bcrypt.compare(req.body.password, user.password)
         if (!validPassword) return res.status(400).send("Password is wrong,disclosing that information just for testing purposes")
 
-        // create and assign a token
+        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET) // .toString()
 
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET).toString()
-        console.log(token)
-
-        res.status(200).cookie('token', token, {
-            expires: new Date(Date.now() + 604800000),
-            secure: false, // if we are not using https
-            httpOnly: true,
-        })
+        res
+            .status(200)
+            .cookie('auth-token', token, {
+                expires: new Date(Date.now() + 604800000),
+                secure: false, // if we are not using https
+                httpOnly: true,
+            })
+            /*         .send(user) */
+            .send({
+                user: user, message: "login successful"
+            })
     } catch (err) {
         res.send(err)
     }
@@ -70,27 +69,34 @@ exports.userLogout = async (req, res) => {
     res.setCookie("auth-token", "").send({ message: "logged out succesfully" })
 }
 
-
-/* 
-exports.getUsers = async (req, res, next) => {
-    try {
-      const users = await User.find()
-        .sort('lastName')
-        .select('-password -__v ');
-      res.status(200).send(users);
-    } catch (e) {
-      next(e);
-    }
-  }; */
-
 exports.getUser = async (req, res, next) => {
     try {
-        const user = await User.findById(req.params.id).select(
-            '-password -__v'
-        );
+        const user = await User
+            .findById(req.params.id)
+            .select('-password -__v')
+        /*             .populate("") */
         if (!user) throw new createError.NotFound();
         res.status(200).send(user);
     } catch (e) {
         next(e);
     }
 };
+
+exports.updateUser = async (req, res, next) => {
+    try {
+        const { firstName, lastName, userName, avatar } = req.body
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: { firstName, lastName, userName, avatar },
+            },
+            {
+                new: true
+            }
+        );
+        console.log(user)
+        res.status(200).send(user)
+    } catch (e) {
+        next(e);
+    }
+}
